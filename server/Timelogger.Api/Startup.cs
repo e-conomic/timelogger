d3b1750c1be08eef883571f29456ea9a5825b438
Calin Laurentiu Ilie <calinilielaur@gmail.com>
2020-09-28 22:22:29 +0200

    bump server to .net core 3.1

--- server/Timelogger.Api/Startup.cs
+++ server/Timelogger.Api/Startup.cs
@@ -4,16 +4,17 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Hosting;
 using Timelogger.Entities;
 
 namespace Timelogger.Api
 {
 	public class Startup
 	{
-		private readonly IHostingEnvironment _environment;
+		private readonly IWebHostEnvironment _environment;
 		public IConfigurationRoot Configuration { get; }
 
-		public Startup(IHostingEnvironment env)
+		public Startup(IWebHostEnvironment env)
 		{
 			_environment = env;
 
@@ -29,40 +30,45 @@ namespace Timelogger.Api
 		public void ConfigureServices(IServiceCollection services)
 		{
 			// Add framework services.
-			services.AddDbContext<ApiContext>(opt => opt.UseInMemoryDatabase());
+			services.AddDbContext<ApiContext>(opt => opt.UseInMemoryDatabase("e-conomic interview"));
+			services.AddLogging(builder =>
+			{
+				builder.AddConsole();
+				builder.AddDebug();
+			});
+
+			services.AddMvc(options => options.EnableEndpointRouting = false);
 
 			if (_environment.IsDevelopment())
 			{
 				services.AddCors();
 			}
-			
-			services.AddMvc();
 		}
 
-		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-		public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
 		{
-			loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-			loggerFactory.AddDebug();
-
 			if (env.IsDevelopment())
 			{
 				app.UseCors(builder => builder
-					.AllowAnyOrigin()
 					.AllowAnyMethod()
 					.AllowAnyHeader()
+					.SetIsOriginAllowed(origin => true)
 					.AllowCredentials());
 			}
 
 			app.UseMvc();
 
-			// Seed "database" with example data
-			var context = app.ApplicationServices.GetService<ApiContext>();
-			AddExampleData(context);
+
+			var serviceScopeFactory = app.ApplicationServices.GetService<IServiceScopeFactory>();
+			using (var scope = serviceScopeFactory.CreateScope())
+			{
+				SeedDatabase(scope);
+			}
 		}
 
-		private static void AddExampleData(ApiContext context)
+		private static void SeedDatabase(IServiceScope scope)
 		{
+			var context = scope.ServiceProvider.GetService<ApiContext>();
 			var testProject1 = new Project
 			{
 				Id = 1,
