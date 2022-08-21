004e312ad7aac7126635d0021d9cabaad0a955c3
Claudiu Hojda <claudiu.hojda@visma.com>
2022-08-21 22:17:27 +0300

    Bump dependencies and format files

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,18 +1,20 @@
-import * as React from 'react';
-import Projects from './views/Projects';
-import './tailwind.generated.css';
+import * as React from "react";
+import Projects from "./views/Projects";
+import "./style.css";
 
 export default function App() {
     return (
         <>
             <header className="bg-gray-900 text-white flex items-center h-12 w-full">
                 <div className="container mx-auto">
-                   <a className="navbar-brand" href="/">Timelogger</a>
+                    <a className="navbar-brand" href="/">
+                        Timelogger
+                    </a>
                 </div>
             </header>
-            
+
             <main>
-                <div className="container mx-auto">                      
+                <div className="container mx-auto">
                     <Projects />
                 </div>
             </main>
--- client/src/app/api/projects.ts
+++ client/src/app/api/projects.ts
@@ -1,6 +1,6 @@
-const BASE_URL = 'http://localhost:3001/api';
+const BASE_URL = "http://localhost:3001/api";
 
 export async function getAll() {
-	const response = await fetch(`${BASE_URL}/projects`);
-	return response.json();
+    const response = await fetch(`${BASE_URL}/projects`);
+    return response.json();
 }
